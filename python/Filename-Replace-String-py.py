d4413b9aff1fc62fafdbb382ab4b2de4df69d44e
Fearherbs1 <fearherbs1@gmail.com>
2021-04-10 11:03:22 -0400

    add filename replace string script

---
+++ python/Filename-Replace-String-py.py
@@ -0,0 +1,20 @@
+# strips a specific string from the filename for all files in the directory the script is run in.
+import os
+
+
+def process_files():
+    # Gather info
+    current_dir = os.getcwd()
+    target_string = input("Enter the string you want to remove: ")
+    print(f"You Entered: {target_string}")
+
+    # iterate through our files and rename them
+    for count, filename in enumerate(os.listdir(current_dir)):
+        if target_string in filename:
+            print(f"Processing {filename}")
+            new_filename = filename.replace(target_string, "")
+            os.rename(filename, new_filename)
+    print("Done")
+
+
+process_files()
